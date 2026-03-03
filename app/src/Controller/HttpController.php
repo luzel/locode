0d77b45e42a0e4152afe484d971e867e22c4dc6d
Luka Uzel <luka.uzel@bornfight.com>
2026-03-03 11:40:15 +0100

    http har

---
+++ app/src/Controller/HttpController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HttpController extends AbstractController {
+
+	#[Route( '/http/har', name: 'app_http_har' )]
+	public function har(): Response {
+		return $this->render(
+			'http/har.html.twig',
+			array(
+				'controller_name' => 'HttpController',
+				'title'           => 'HAR Viewer',
+			)
+		);
+	}
+}
